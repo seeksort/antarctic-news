5a50d9ed9a6d41d8d2b5439bde0e18f4ce461f08
Kristin Faner <kfaner@gmail.com>
2017-05-03 20:42:16 -0500

    configs, first react component

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  devtool: "source-map",
+  context: __dirname,
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
